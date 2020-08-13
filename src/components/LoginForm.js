359d9c35de9d024d42950ee9b82bc15c8ac2cc83
Saleem Saleh <salem1234saleem@gmail.com>
2020-08-13 08:36:17 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -20,9 +20,11 @@ class LoginForm extends React.Component {
   Submit = event => {
     event.preventDefault()
       if(!this.state.username || !this.state.password){
-
-      }
+    
+      }else{
         this.props.handleLogin(this.state);
+      }
+        
   }
   render() {
     return (
